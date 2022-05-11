ae12d8d591ae08b49bda2e2008966531e72d97fe
TA-Sakin <tasakin98@gmail.com>
2022-05-11 16:49:10 +0600

    updated quantity

--- src/Components/ProductDetail/ProductDetail.js
+++ src/Components/ProductDetail/ProductDetail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import Spinner from "../Spinner/Spinner";
 
 const ProductDetail = () => {
@@ -11,9 +12,26 @@ const ProductDetail = () => {
       .then((res) => res.json())
       .then((data) => setBook(data));
   }, [id]);
+
   if (book.length === 0) {
     return <Spinner></Spinner>;
   }
+  const handleDelivered = () => {
+    let quantity = book.quantity;
+    quantity = quantity + 1;
+    const updateQuantity = { quantity };
+
+    fetch(`http://localhost:5000/books/${id}`, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(updateQuantity),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setBook({ ...book, quantity: quantity });
+        toast("One more book added");
+      });
+  };
   return (
     <div className="container mt-5" style={{ minHeight: "66vh" }}>
       <div className="card mb-3 mx-auto" style={{ maxWidth: "800px" }}>
@@ -29,14 +47,17 @@ const ProductDetail = () => {
             <div className="card-body">
               <h5 className="card-title">{book.name}</h5>
               <p className="card-text">{book.description}</p>
-              <p className="card-text">{book.supplier}</p>
-              <p className="card-text">{book.price}</p>
-              <p className="card-text">{book.quantiy}</p>
-              <button className="btn btn-primary">Delivered</button>
+              <p className="card-text">Supplier: {book.supplier}</p>
+              <p className="card-text">Price: ${book.price}</p>
+              <p className="card-text">Quantity: {book.quantity}</p>
+              <button className="btn btn-primary" onClick={handleDelivered}>
+                Delivered
+              </button>
             </div>
           </div>
         </div>
       </div>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
